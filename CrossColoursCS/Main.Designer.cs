44c5ff4b514324cb7ba2524a5d6d7316a7d88965
Mihajlo <nesic.srbija98@gmail.com>
2017-04-03 15:44:29 +0100

    Initial commit

---
+++ CrossColoursCS/Main.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CrossColoursCS
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tlp_game = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.pcb_logo = new System.Windows.Forms.PictureBox();
+            this.nud_rows = new System.Windows.Forms.NumericUpDown();
+            this.nud_cols = new System.Windows.Forms.NumericUpDown();
+            this.btn_generate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_rows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_cols)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlp_game
+            // 
+            this.tlp_game.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlp_game.ColumnCount = 6;
+            this.tlp_game.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.Location = new System.Drawing.Point(12, 69);
+            this.tlp_game.Name = "tlp_game";
+            this.tlp_game.RowCount = 6;
+            this.tlp_game.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_game.Size = new System.Drawing.Size(460, 438);
+            this.tlp_game.TabIndex = 0;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_reset.Location = new System.Drawing.Point(16, 513);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 1;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // pcb_logo
+            // 
+            this.pcb_logo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pcb_logo.Image = global::CrossColoursCS.Properties.Resources.brainbashers;
+            this.pcb_logo.Location = new System.Drawing.Point(12, 2);
+            this.pcb_logo.Name = "pcb_logo";
+            this.pcb_logo.Size = new System.Drawing.Size(460, 61);
+            this.pcb_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcb_logo.TabIndex = 2;
+            this.pcb_logo.TabStop = false;
+            this.pcb_logo.Click += new System.EventHandler(this.pcb_logo_Click);
+            // 
+            // nud_rows
+            // 
+            this.nud_rows.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nud_rows.Location = new System.Drawing.Point(263, 516);
+            this.nud_rows.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.nud_rows.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_rows.Name = "nud_rows";
+            this.nud_rows.Size = new System.Drawing.Size(58, 20);
+            this.nud_rows.TabIndex = 3;
+            this.nud_rows.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // nud_cols
+            // 
+            this.nud_cols.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nud_cols.Location = new System.Drawing.Point(327, 516);
+            this.nud_cols.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.nud_cols.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_cols.Name = "nud_cols";
+            this.nud_cols.Size = new System.Drawing.Size(58, 20);
+            this.nud_cols.TabIndex = 4;
+            this.nud_cols.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_generate.Location = new System.Drawing.Point(391, 513);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(75, 23);
+            this.btn_generate.TabIndex = 5;
+            this.btn_generate.Text = "Generate";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 548);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.nud_cols);
+            this.Controls.Add(this.nud_rows);
+            this.Controls.Add(this.pcb_logo);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.tlp_game);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(334, 405);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cross Colours CS";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_rows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_cols)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlp_game;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.PictureBox pcb_logo;
+        private System.Windows.Forms.NumericUpDown nud_rows;
+        private System.Windows.Forms.NumericUpDown nud_cols;
+        private System.Windows.Forms.Button btn_generate;
+    }
+}
+
---
+++ CrossColoursCS/Main.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrossColoursCS
+{
+    public partial class Main : Form
+    {
+        private Color CCGreen = Color.FromArgb(255, 51, 255, 0);
+
+        private Button[,] ButtonMatrix;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            CreateGame(6, 6);
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tlp_game.RowCount; i++)
+                for (int j = 0; j < tlp_game.ColumnCount; j++)
+                    ButtonMatrix[i, j].BackColor = Color.Red;
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            int r = Convert.ToInt32(nud_rows.Value);
+            int c = Convert.ToInt32(nud_cols.Value);
+            CreateGame(r, c);
+        }
+
+        private void pcb_logo_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.brainbashers.com/crosscolours.asp");
+            System.Diagnostics.Process.Start("https://mihajlonesic.gitlab.io/");
+        }
+
+        private void CreateGame(int row, int col)
+        {
+            tlp_game.Controls.Clear();
+
+            tlp_game.RowStyles.Clear();
+            tlp_game.ColumnStyles.Clear();
+
+            tlp_game.RowCount = row;
+            tlp_game.ColumnCount = col;
+
+            ButtonMatrix = new Button[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    ButtonMatrix[i, j] = new Button();
+                    ButtonMatrix[i, j].BackColor = Color.Red;
+                    ButtonMatrix[i, j].Padding = new Padding(0);
+                    ButtonMatrix[i, j].Margin = new Padding(0);
+                    ButtonMatrix[i, j].Anchor = (AnchorStyles.Bottom | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Left);
+                    ButtonMatrix[i, j].Click += new EventHandler(CCButtonClickHandler);
+                    tlp_game.Controls.Add(ButtonMatrix[i, j], j, i);
+                }
+            }
+
+            for (int i = 1; i <= tlp_game.RowCount; i++)
+            {
+                tlp_game.RowStyles.Add(new RowStyle(SizeType.Percent, 1));
+            }
+            for (int i = 1; i <= tlp_game.ColumnCount; i++)
+            {
+                tlp_game.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 1));
+            }
+        }
+
+        private void CCButtonClickHandler(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+
+            if (b != null)
+            {
+                ToggleColor(b);
+
+                TableLayoutPanelCellPosition pos = tlp_game.GetCellPosition(b);
+
+                if (pos.Row < (tlp_game.RowCount))
+                {
+                    Button neig1 = (Button)tlp_game.GetControlFromPosition(pos.Column, pos.Row + 1);
+                    if (neig1 != null)
+                        ToggleColor(neig1);
+
+                    if (pos.Row > 0)
+                    {
+                        Button neig2 = (Button)tlp_game.GetControlFromPosition(pos.Column, pos.Row - 1);
+                        if (neig2 != null)
+                            ToggleColor(neig2);
+                    }
+                }
+
+                if (pos.Column < (tlp_game.ColumnCount))
+                {
+                    Button neig3 = (Button)tlp_game.GetControlFromPosition(pos.Column + 1, pos.Row);
+                    if (neig3 != null)
+                        ToggleColor(neig3);
+
+                    if (pos.Column > 0)
+                    {
+                        Button neig4 = (Button)tlp_game.GetControlFromPosition(pos.Column - 1, pos.Row);
+                        if (neig4 != null)
+                            ToggleColor(neig4);
+                    }
+                }
+
+            }
+
+            if (Win())
+                MessageBox.Show("You are victorious!", "Win", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void ToggleColor(Button b)
+        {
+            if (b.BackColor == Color.Red)
+                b.BackColor = CCGreen;
+            else b.BackColor = Color.Red;
+        }
+
+        private bool Win()
+        {
+            for (int i = 0; i < tlp_game.RowCount; i++)
+                for (int j = 0; j < tlp_game.ColumnCount; j++)
+                    if (ButtonMatrix[i, j].BackColor != CCGreen)
+                        return false;
+            return true;
+        }
+    }
+}
